e1b99fd0ae05d6b528ec23a436094dde38501487
Artem <polorion1@gmail.com>
2024-10-11 20:42:42 +0300

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-// import './App.css'
-//
-// import {InfoBusiness} from "./components/InfoBusiness/InfoBusiness.jsx";
-// import {Route, Routes, useLocation} from "react-router-dom";
-// import Layout from "./components/layout/Layout.jsx";
-// import {ChoicePlatform} from "./components/ChoisePlatform/ChoicePlatform.jsx";
-// import {CSSTransition, TransitionGroup} from "react-transition-group";
-//
-// function App() {
-//     const location = useLocation();
-//     return (
-//         <div className="container">
-//             <TransitionGroup>
-//                 <CSSTransition key={location.key} classNames="fade" timeout={300}>
-//                     <Routes location={location}>
-//                         <Route path={"/"} element={<Layout/>}>
-//                             <Route path={""} element={<InfoBusiness/>}/>
-//                             <Route path={"next"} element={<ChoicePlatform/>}/>
-//                         </Route>
-//                     </Routes>
-//                 </CSSTransition>
-//             </TransitionGroup>
-//         </div>
-//     )
-// }
-//
-// export default App
 
 import React from "react";
 import {
